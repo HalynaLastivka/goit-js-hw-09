44950f7593a9c4c15df73385e16679c122d3537d
HalynaLastivka <127863052+HalynaLastivka@users.noreply.github.com>
2023-07-30 21:06:07 +0000

    Deploying to gh-pages from @ HalynaLastivka/goit-js-hw-09@82dae603dcb64932d6ee5cbbe9d499ec06633b52 🚀

--- 01-color-switcher.b309f2aa.js
+++
@@ -1,2 +0,0 @@
-!function(){var t=document.querySelector("[data-start]"),e=document.querySelector("[data-stop]");t.addEventListener("click",(function(){t.disabled||(e.disabled=!1,t.disabled=!0,a=setInterval((function(){document.body.style.background="#".concat(Math.floor(16777215*Math.random()).toString(16).padStart(6,0))}),1e3))})),e.addEventListener("click",(function(){e.disabled||(e.disabled=!0,t.disabled=!1,clearInterval(a))}));var a=0}();
-//# sourceMappingURL=01-color-switcher.b309f2aa.js.map
